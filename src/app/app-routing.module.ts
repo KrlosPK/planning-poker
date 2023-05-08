4b7ae22cd274dcf249357bf2316d0a57de4eb866
Carlos Morales <carlos.morales@pragma.com.co>
2023-05-08 17:57:17 -0500

    fixed loading route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { CreateGameComponentComponent } from './components/create-game-component/create-game-component.component';
 import { GameComponentComponent } from './components/game-component/game-component.component';
 import { PageNotFoundComponentComponent } from './components/page-not-found-component/page-not-found-component.component';
+import { LoadingPageComponentComponent } from './components/loading-page-component/loading-page-component.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: 'loading-page', pathMatch: 'full' },
+  { path: 'loading-page', component: LoadingPageComponentComponent },
   { path: 'create-game', component: CreateGameComponentComponent },
+  { path: '404', component: PageNotFoundComponentComponent },
   { path: 'game', component: GameComponentComponent },
   { path: '**', component: PageNotFoundComponentComponent }
 ];
