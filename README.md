c102181ed53c027399ae5dbe3541c9d8c7c2fd22
Carlos Morales <carlos.morales@pragma.com.co>
2023-05-09 09:39:57 -0500

    navigate to create game after load page

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Poker app
+# Planning Poker app
+
+### [Visita el sitio](https://planning-poker-app.netlify.app)
--- src/app/components/navbar-component/navbar-component.component.ts
+++ src/app/components/navbar-component/navbar-component.component.ts
@@ -1,13 +1,8 @@
-import { Location } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navbar-component',
   templateUrl: './navbar-component.component.html',
-  styleUrls: ['./navbar-component.component.css']
+  styleUrls: ['./navbar-component.component.css'],
 })
-export class NavbarComponentComponent {
-  ngOnInit():void {
-    console.log("object");
-  }
-}
+export class NavbarComponentComponent {}
